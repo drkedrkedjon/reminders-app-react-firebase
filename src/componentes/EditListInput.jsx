fc62d100fda3142064396ca8f4a84cad47b45413
Sasa Savic Perusina <sasa@bubulazy.com>
2023-05-30 13:59:56 +0100

    Finished delay 3sec functionality on delete list

--- src/componentes/EditListInput.jsx
+++ src/componentes/EditListInput.jsx
@@ -1,17 +1,20 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 
-/* eslint-disable react/prop-types */
 export default function EditListInput({ lista, handleNewName, deleteList }) {
   const id = lista[0];
   const oldName = lista[1].name;
   const numItems = lista[1].items ? Object.keys(lista[1].items).length : 0;
 
   const [listName, setListName] = useState(oldName);
+  const [isDeletingList, setIsDeletingList] = useState(false);
 
   function handleListName(e) {
     setListName(e.target.value);
   }
 
+  // Handle cambio de nombre
   useEffect(() => {
     if (oldName === listName) {
       return;
@@ -22,9 +25,23 @@ export default function EditListInput({ lista, handleNewName, deleteList }) {
     return () => clearTimeout(timeout);
   }, [listName]);
 
+  //  Handle aplazar o deley delete
+  useEffect(() => {
+    if (!isDeletingList) {
+      return;
+    }
+    const cancelTimeout = setTimeout(() => deleteList(id), 3000);
+    return () => clearTimeout(cancelTimeout);
+  }, [isDeletingList]);
+
+  const btnRedCancel = isDeletingList ? "cancel-" : "";
+
   return (
     <div className="list-form-component">
-      <button onClick={() => deleteList(id)} className="delete-btn"></button>
+      <button
+        onClick={() => setIsDeletingList(!isDeletingList)}
+        className={`${btnRedCancel}delete-btn`}
+      ></button>
       <input type="text" onChange={handleListName} value={listName} />
       <p className="list-form--num-items">{numItems}</p>
     </div>
