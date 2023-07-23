84dbbe72012dd762001a121e1fa5cf5f22e99cae
Sasa Savic Perusina <sasa@bubulazy.com>
2023-07-23 16:18:00 +0100

    Home default lists finished

--- src/componentes/BarraHeader.jsx
+++ src/componentes/BarraHeader.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import { Home, Settings, List } from "react-feather";
+import { HomeDisplayTypeContext } from "../scripts/DataContexts";
+import { useContext } from "react";
 
 export default function BarraHeader() {
+  const { setHomeType } = useContext(HomeDisplayTypeContext);
   return (
     <header className="header-container">
       <Link to={"/edit-lists"}>
         <List size={18} />
       </Link>
-      <Link to={"/"}>
+      <Link onClick={() => setHomeType("lists")} to={"/"}>
         <Home size={22} />
       </Link>
       <Link to={"/settings"}>
